c1d5623bc93a1557d4ab603a39fbbaf37fc825f1
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2012-01-26 14:49:34 +0100

    la boite à mousse tâche

--- Display/Boites/DBoites.php
+++ Display/Boites/DBoites.php
@@ -2,5 +2,32 @@
 class DBoites
 {
 	const nom = 'Boites';
+	
+	public $data = null;
+
+	public function show()
+	{
+		CHead::addJs('raphael-min');
+		CHead::addJs('Plot_js/raphael_boxplot');
+
+		$tab = $this->data;
+
+		$tds="<tr class='données 1'>";
+		for($i=0; $i<count($tab); $i++){
+			$tds.="<td>".$tab[$i]."</td>";
+		}
+		$tds.="</tr>";
+		echo "<div id='first' style='width:640px;height:480px;'></div>"
+					."<div id='data' style='display:none;'><table>"
+					."<tr>
+						<th>Min</th>
+						<th>Quartile 1</th>
+						<th>Moyenne</th>
+						<th>Quartile 3</th>
+						<th>Max</th>
+					</tr>"
+					.$tds."</table></div>"
+					."<script>showBoxPlot();</script>";
+	}
 }
 ?>
