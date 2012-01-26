ea122dc0271493396e5990bed53c04f49d9d14a5
Thomas Moreau <moreau@polytech.unice.fr>
2012-01-26 11:03:40 +0100

    tableau radom trie youpi

---
+++ View/TestGraph.php
@@ -0,0 +1,39 @@
+<?php
+echo <<<END
+<html>
+<head>
+	<link href="/InspecteurDeryque/Css/bootstrap.min.css" media="screen" rel="Stylesheet" type="text/css" />
+	<link href="/InspecteurDeryque/Css/application.css" media="screen" rel="Stylesheet" type="text/css" />
+	<link href="/InspecteurDeryque/Css/Dashboard.css" media="screen" rel="Stylesheet" type="text/css" />
+</head>
+<body>
+END;
+echo "<pre>";
+print_r(TestRandom::TriPoint(TestRandom::tableauRandom()));
+echo "</pre>";
+
+
+echo <<<END
+</body>
+</html>
+END;
+
+?>
+
+<?php
+
+class TestRandom{
+	public static function tableauRandom(){
+		for($i = 0 ; $i < 10 ; $i++){
+			$tab["abscisse"][$i] = rand(1, 100);
+			$tab["ordonee"][$i] = rand(1, 100);
+		}
+		return $tab;
+	}
+	public static function TriPoint($tab){
+		array_multisort($tab["abscisse"], SORT_ASC, $tab["ordonee"]);
+		return $tab;
+	}
+}
+
+?>
