f636adaccb05e1fa8b3444518f2b4d7411cc94f7
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2012-01-27 16:39:25 +0100

    maj pour que ça passe mieux sur tablette

--- View/Plot_templates/realtime-json.php
+++ View/Plot_templates/realtime-json.php
@@ -1,11 +1,5 @@
 <?php
 header("Content-type: text/javascript"); 
 
-#function tableauRandom($nb = 10, $max = 100){
-#	for($i = 1 ; $i < $nb ; $i++){
-#		$entry[$i]=rand(1,$max);
-#	}
-#	return $entry;
-#}
-echo json_encode( rand(1,50) );
+echo json_encode( rand(10,40) );
 ?>
