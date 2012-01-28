e4113db48794f53e352141005e5527048537c7b6
Kevin Rocher <krocher@polytech.unice.fr>
2012-01-28 17:15:55 +0100

    la map fonctionne

--- Js/carte.js
+++ Js/carte.js
@@ -1,5 +1,36 @@
 	function initialiser() {
-		var latlng = new google.maps.LatLng((46.781367900048+46.778605381016)/2, (6.6401992834884+6.6588674582321)/2);
+		var tables = document.getElementById("data").getElementsByTagName("table");
+		var trs,i,j;
+		var timestamp = [];
+		var lat = [];
+		var lon = [];
+		var vit = [];
+		for(i=0;i<tables.length;i++){
+			trs=tables[i].getElementsByTagName("tr");
+			var dataintr = trs[0].getElementsByTagName("td");
+			for(j=0;j<dataintr.length;j++){
+				timestamp.push(parseFloat(dataintr[j].innerHTML));
+			}
+
+			dataintr = trs[1].getElementsByTagName("td");
+			for(j=0;j<dataintr.length;j++){
+				lat.push(parseFloat(dataintr[j].innerHTML));
+			}
+			
+			dataintr = trs[2].getElementsByTagName("td");
+			for(j=0;j<dataintr.length;j++){
+				lon.push(parseFloat(dataintr[j].innerHTML));
+			}
+		}
+		var lastlat = 0, lastlon = 0, firstlat = 0, firstlon = 0;
+		if (lat.length > 0 && lon.length == lat.length)
+		{
+			lastlat = lat[lat.length-1];
+			lastlon = lon[lon.length-1];
+			firstlat = lat[0];
+			firstlon = lon[0];
+		}
+		var latlng = new google.maps.LatLng((firstlat+lastlat)/2, (lastlon+firstlon)/2);
 		var options = {
 			center: latlng,
 			zoom: 16,
@@ -7,36 +38,52 @@
 			scaleControl: true
 		};
 		var carte = new google.maps.Map(document.getElementById("carte"), options);
-
-		//polyligne
-		var parcoursBus = [
-			new google.maps.LatLng(46.781367900048, 6.6401992834884),
-			new google.maps.LatLng(46.780821285011, 6.6416348016222),
-			new google.maps.LatLng(46.780496546047, 6.6421830461926),
-			new google.maps.LatLng(46.779835306991, 6.6426765713417),
-			new google.maps.LatLng(46.777748677169, 6.6518819126808),
-			new google.maps.LatLng(46.778027878803, 6.6541349682533),
-			new google.maps.LatLng(46.778484884759, 6.6557324922045),
-			new google.maps.LatLng(46.778752327087, 6.6573654211838),
-			new google.maps.LatLng(46.778605381016, 6.6588674582321)
-		];
-	
-		var traceParcoursBus = new google.maps.Polyline({
-			path: parcoursBus,
-			strokeColor: "#FF0000",
-			strokeOpacity: 1.0,
-			strokeWeight: 2
-		});
+		var maxvit = 0;
+		var minvit = Number.MAX_VALUE;
+		for(i=1;i<lat.length;i++){
+			var dist = Math.sqrt(Math.pow((lat[i]-lat[i-1]),2) + Math.pow((lon[i]-lon[i-1]),2))
+			var tps = timestamp[i] - timestamp[i-1];
+			if (maxvit < dist /tps)
+				maxvit = dist /tps;
+			if (minvit > dist /tps)
+				minvit = dist /tps;
+			vit.push(dist/tps);
+		}
 		
-		traceParcoursBus.setMap(carte);
+		//polyligne
+		for(i=1;i<lat.length;i++){
+			var parcours = [];
+			parcours.push(new google.maps.LatLng(lat[i-1], lon[i-1]));
+			parcours.push(new google.maps.LatLng(lat[i], lon[i]));
+			var ratio = (vit[i-1]-minvit)/(maxvit-minvit);
+			var red = Math.floor(255 * ratio);
+			var blue = Math.floor(255 * (1 - ratio));
+			red = red.toString(16);
+			blue = blue.toString(16);
+			if (red == "0")
+				red = "00";
+			if (blue == "0")
+				blue = "00";
+			var color = "#" + red.toUpperCase() + "00" + blue.toUpperCase();
+			var traceparcours = new google.maps.Polyline({
+				path: parcours,
+				strokeColor: color,
+				strokeOpacity: 1.0,
+				strokeWeight: 2
+			});
+		
+			traceparcours.setMap(carte);
+		}
 
 		var marqueur = new google.maps.Marker({
-			position: new google.maps.LatLng(46.781367900048, 6.6401992834884),
-			map: carte
+			position: new google.maps.LatLng(firstlat, firstlon),
+			map: carte,
+			title: "Départ"
 		});
 		var marqueur = new google.maps.Marker({
-			position: new google.maps.LatLng(46.778605381016, 6.6588674582321),
-			map: carte
+			position: new google.maps.LatLng(lastlat, lastlon),
+			map: carte,
+			title: "Arrivée"
 		});
 	}
 
