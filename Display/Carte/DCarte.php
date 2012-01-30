13bd05c8a02f195c00a70f226d7b428430f1992c
Kevin Rocher <krocher@polytech.unice.fr>
2012-01-30 11:54:51 +0100

    la map bug plus

--- Display/Carte/DCarte.php
+++ Display/Carte/DCarte.php
@@ -7,12 +7,16 @@ class DCarte extends DAbstract
 	{
 		CHead::addJs('carte');
 		CHead::addJs('http://maps.google.com/maps/api/js?sensor=false');
-		echo '<div id="carte" style="width:960px; height:600px"></div>';
 		$timestamp="<tr>";
 		$lat="<tr>";
 		$lon="<tr>";
 		foreach ($this->data as $data)
 		{
+			if (!$data['timestamp'] || !$data['lat'] || !$data['lon'])
+			{
+				new CMessage('Impossible d\'afficher la carte', 'warning');
+				return;
+			}
 			$timestamp .="<td>".$data['timestamp']."</td>";
 			$lat .="<td>".$data['lat']."</td>";
 			$lon .="<td>".$data['lon']."</td>";
@@ -20,6 +24,7 @@ class DCarte extends DAbstract
 		$timestamp.="</tr>";
 		$lat.="</tr>";
 		$lon.="</tr>";
+		echo '<div id="carte" style="width:960px; height:600px"></div>';
 		echo "<div id='data' style='display:none;'><table>\n"
 					.$timestamp."\n".$lat."\n".$lon."\n</table>\n</div>";
 	}
