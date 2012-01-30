e5f2bc0313c80ec95385776b289c23febf4747b9
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2012-01-30 12:09:22 +0100

    Les boites remarchent

--- Display/Boites/DBoites.php
+++ Display/Boites/DBoites.php
@@ -2,13 +2,45 @@
 class DBoites extends DAbstract
 {
 	const nom = 'Boites';
-
+	
+	private static function quartiles($entry){
+		// FIXME code en dur, affiche la température
+		$type='temperature';
+		
+		$subtab=array();
+		foreach($entry as $c=>$key){
+			array_push($subtab,$key[$type]);
+		}
+		
+		sort($subtab,SORT_NUMERIC);
+		$tab=array($subtab[0],0,0,0,end($subtab));
+		$i_max=count($subtab);
+		for($i=0;$i<$i_max;$i++){
+			// 1er quartile
+			if ($tab[1] == 0 && $subtab[$i] > round($tab[4]/4,0, PHP_ROUND_HALF_UP) ){
+				$tab[1] = $subtab[$i];
+			}
+			// 3ème quartile
+			if ($tab[3] == 0 && $subtab[$i] > round($tab[4]*3/4,0, PHP_ROUND_HALF_UP) ){
+				$tab[3] = $subtab[$i];
+			}
+		}
+		// Valeur médiane
+		if ($i_max%2!=0){
+			$tab[2]=$subtab[ ($i_max+1)/2 ];
+		} else {
+			$tab[2]=($subtab[round( ($i_max+1)/2,0, PHP_ROUND_HALF_DOWN)]
+					+$subtab[round( ($i_max+1)/2,0, PHP_ROUND_HALF_UP)  ])/ 2 ;
+		}
+		return $tab;
+	}
+	
 	public function show()
 	{
 		CHead::addJs('raphael-min');
 		CHead::addJs('Plot_js/raphael_boxplot');
 
-		$tab = $this->data;
+		$tab = self::quartiles($this->data);
 		
 		$tds="<tr class=''>";
 		$i_max=count($tab);
