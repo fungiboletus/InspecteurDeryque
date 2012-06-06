7a4c99006eb7e6c2c35abd54aad5a9967301fdd1
Thomas Moreau <moreau@polytech.unice.fr>
2012-06-06 10:23:27 +0200

    debut de l'interface REST

---
+++ Ctrl/RestJson.php
@@ -0,0 +1,34 @@
+<?php
+class RestJson
+{
+	public function index(){
+        CNavigation::setTitle('RestJson');
+        CNavigation::setDescription('REST interface');
+    }
+    
+    /**
+    * sends a Json message which contains a list of reports
+    */
+    public function reports(){
+    	$reports = DataMod::getReleves($_SESSION['bd_id']);
+    	$arr = array();
+    	foreach($reports as $report){
+    		$arr[$report['name']] = $report['description'];
+    	}
+    	echo json_encode($arr);
+    }
+    
+    public function resume($name){
+    	//TO DO
+    }
+    
+    public function data($name){
+    
+    }
+    
+    public function data_dt($name){
+    
+    }
+}
+
+?>
---
+++ Ctrl/RestXML.php
@@ -0,0 +1,29 @@
+<?php
+class RestJson
+{
+	public function index(){
+        CNavigation::setTitle('RestJson');
+        CNavigation::setDescription('REST interface');
+    }
+    
+    /**
+    * sends an XML message which contains a list of reports
+    */
+    public function reports(){
+    	
+    }
+    
+    public function resume($name){
+    	//TO DO
+    }
+    
+    public function data($name){
+    
+    }
+    
+    public function data_dt($name){
+    
+    }
+}
+
+?>
