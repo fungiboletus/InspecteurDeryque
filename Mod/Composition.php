4c28553b34ece4ff0c1cdcf7f51459b4046da487
Zarmakuizz <generalzarmakuizz@hotmail.fr>
2012-06-11 12:09:26 +0200

    traitement de Composition en cours, avec de jolis FIXME parci parlà

--- Mod/Composition.php
+++ Mod/Composition.php
@@ -1,18 +1,24 @@
 <?php
 
 /**
- * Associates some compositions to a statement.
+ * A composition is a group of selections associated to a statement.
+ * This class is used to display multiple statements into the same graphic,
+ *          even though doing it server-side and storing this into the database
+ *          is stupid as fuck.
+ * @see Selection
  */
 class Composition {
 
-    private $_compositionBean;
-    private $_statementBean;
+    private $_compositionBean; /**< storing the current composition */
+    private $_statementBean; /**< storing the targeted statement */
 
     /**
-     * Add a composition to a statement.
+     * Add to a statement a composition.
+     * @param $sName Name of the statement.
+     * @param $name Name of the composition.
      */
-    public function __construct($rname, $name) {
-        $beans = R::find('releve', "name = ?", array($rname));
+    public function __construct($sName, $name) {
+        $beans = R::find('releve', "name = ?", array($sName));
 
         foreach ($beans as $bean) {
             $this->_statementBean = $bean;
@@ -20,7 +26,7 @@ class Composition {
         }
 
         if ($this->_statementBean === NULL) {
-            $beans = R::find('multi_releve', "name = ?", array($rname));
+            $beans = R::find('multi_releve', "name = ?", array($sName));
 
             foreach ($beans as $bean) {
                 $this->_statementBean = $bean;
@@ -29,55 +35,50 @@ class Composition {
         }
 
         $this->_compositionBean = R::dispense('composition');
-
         $this->_compositionBean->name = $name;
-
         $this->_compositionBean->releve_id = $this->_statementBean->getID();
         $this->_compositionBean->releve_type = $this->_statementBean->getMeta('type');
-
     }
 
     /**
-     * Add a selection (bean) to a composition.
+     * Add a selection to a composition using a selection bean
+     * @param $selectionBean The selection to associate to the current composition.
      */
     public function addSelection($selectionBean) {
         $selectionBean->composition = $this->_compositionBean;
-
         R::store($selectionBean);
     }
 
     /**
      * Add a selection (obtained from data) to a composition.
+     * @param $graphName Name of the graphic.
+     * @param $begin The timestamp of the first data of the selection
+     * @param $end The timestamp of the last data of the selection
      */
-    public function addNewSelection($graphName, $start, $fin) {
-        $selection = new Selection($this->_statementBean->name, $graphName, $start, $fin);
-
+    public function addNewSelection($graphName, $begin, $end) {
+        $selection = new Selection($this->_statementBean->name, $graphName, $begin, $end);
         $selection->save();
-
         $selectionBean = $selection->getBean();
-
         $this->addSelection($selectionBean);
-
     }
 
     /**
      * Store selection into the database.
+     * FIXME seems like a copypasta from Selection
+     * @see Selection
      */
     public function save() {
-
         R::store($this->_statementBean);
-
         R::store($this->_selectionBean);
-
     }
 
     /**
      * Get all compositions associated to a statement.
      */
-    public static function getCompositions($rname) {
-        $statements = R::find('releve', "name = ?", array($rname));
+    public static function getCompositions($sName) {
+        $statements = R::find('releve', "name = ?", array($sName));
         if ($statements == NULL) {
-            $statements = R::find('multi_releve', "name = ?", array($rname));
+            $statements = R::find('multi_releve', "name = ?", array($sName));
         }
 
         foreach ($statements as $statement) {
--- Mod/Selection.php
+++ Mod/Selection.php
@@ -1,18 +1,26 @@
 <?php
 
 /**
- * Associe une ou plusieurs selections à un relevé.
+ * A selection is a subpart of a statement. The goal is to load less data
+ * than a complete statement when the whole statement is not needed
+ * (e.g. the user wants to see yesterday's data and not other days).
+ * This class allows to associate one or more selections to a statement.
+ * @see Composition
  */
 class Selection {
 
-    private $_selectionBean;
-    private $_statementBean;
+    private $_selectionBean; /**< storing the current selection */
+    private $_statementBean; /**< storing the targeted statement */
 
     /**
-     * Ajoute une selection au relevé.
+     * Adds to a statement a selection.
+     * @param $sName Name of the statement.
+     * @param $graphName Name of the graphic.
+     * @param $begin The timestamp of the first data of the selection
+     * @param $end The timestamp of the last data of the selection
      */
-    public function __construct($rname, $graphName, $begin, $end) {
-        $beans = R::find('releve', "name = ?", array($rname));
+    public function __construct($sName, $graphName, $begin, $end) {
+        $beans = R::find('releve', "name = ?", array($sName));
 
         foreach ($beans as $bean) {
             $this->_statementBean = $bean;
@@ -20,7 +28,7 @@ class Selection {
         }
 
         if ($this->_statementBean === NULL) {
-            $beans = R::find('multi_releve', "name = ?", array($rname));
+            $beans = R::find('multi_releve', "name = ?", array($sName));
 
             foreach ($beans as $bean) {
                 $this->_statementBean = $bean;
@@ -40,47 +48,45 @@ class Selection {
     }
 
     /**
-     * Envoie la selection dans la BD.
+     * Store the current selection.
      */
     public function save() {
-
         R::store($this->_statementBean);
-
         R::store($this->_selectionBean);
-
     }
 
     /**
-     * Récupère toutes les selections associées à un relevé (et non attaché à une composition).
+     * Get all selections associated to a statement
+     * that are not associated to a composition.
+     * @param $sName the name of the asked statement.
+     * @return $selections an array of Selection.
+     * FIXME This method could access to another user's statements.
      */
-    public static function getSelections($rname) {
-        $statements = R::find('releve', "name = ?", array($rname));
+    public static function getSelections($sName) {
+        $statements = R::find('releve', "name = ?", array($sName));
         if ($statements == NULL) {
-            $statements = R::find('multi_releve', "name = ?", array($rname));
+            $statements = R::find('multi_releve', "name = ?", array($sName));
         }
         
-        
-        
-
         foreach ($statements as $statement) {
-            
             $values['id'] = $statement->getID();
             $values['type'] = $statement->getMeta('type');
-            
             $selections = R::find ( 'selection', 'releve_id = :id AND releve_type = :type AND composition_id IS NULL',$values );
-            
             return $selections;
         }
-
     }
 
     /**
-     * Récupérer le bean de la sélection.
+     * Get the selection bean.
+     * @return $bean A selection bean.
      */
     public function getBean() {
         return $this->_selectionBean;
     }
     
+    /**
+     * Delete the current selection bean in the database.
+     */
     public function delete() {
         R::trash($this->_selectionBean);
     }
