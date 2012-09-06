d9f9a92b195c8c7e3b8567c599ab95feaa9eedc5
Antoine Pultier <antoine@blogjaune.fr>
2012-09-06 16:46:00 +0200

    calcul automatique du heading lorsqu'il n'est pas présent

--- Display/StreetView/DStreetView.js
+++ Display/StreetView/DStreetView.js
@@ -13,6 +13,9 @@ var DStreetView = function(screen)
 	EventBus.addListeners(this.listeners, this);
 
 	this.database = {};
+
+	// Last point
+	this.last_point = null;
 };
 
 DStreetView.prototype =
@@ -20,22 +23,31 @@ DStreetView.prototype =
 listeners: {
 values: function(detail, obj) {
 
+	var current_point = null;
+	var current_heading = null;
+
 	for (var statement_name in detail) {
 		if (!(statement_name in obj.database)) continue;
 		var data = detail[statement_name];
 
 		if ('heading' in data)
 		{
-			obj.panorama.setPov({
-				heading: data.heading * 180.0 / Math.PI,
-				zoom:1,
-				pitch:0
-			});
+			current_heading = data.heading * 180.0 / Math.PI;
 		}
 		else if (('lat' in  data) && ('lon' in data))
 		{
 			var ll = new google.maps.LatLng(data.lat, data.lon);
 			obj.panorama.setPosition(ll);
+
+			if (current_point !== null)
+			{
+				EventBus.send('error', {
+					status: 'Too much GPS data',
+					message: 'The StreetView visualization can display'
+					+'only one GPS statement'});
+			}
+
+			current_point = ll;
 		}
 		else
 		{
@@ -44,6 +56,21 @@ values: function(detail, obj) {
 				message: 'The map can only show GPS statements'});
 		}
 	}
+
+	// If a heading is not present, create a bad heading with
+	// the last fetched value
+	if (current_heading !== null &&
+		current_point !== null && obj.last_point !== null)
+		current_heading = google.maps.geometry.spherical.computeHeading(
+			obj.last_point, current_point);
+
+	obj.panorama.setPov({
+		heading: (current_heading === null ? 0.0 : current_heading),
+		zoom:1,
+		pitch:0
+	});
+
+	this.last_point = current_point;
 },
 add_statement: function(e, obj) {
 	if (e.box_name != self.name) return;
@@ -56,5 +83,8 @@ del_statement: function(e, obj) {
 
 	if (e.statement_name in obj.database)
 		delete obj.database[e.statement_name];
+
+	this.have_heading = false;
+	this.last_point = null;
 }
 }};
